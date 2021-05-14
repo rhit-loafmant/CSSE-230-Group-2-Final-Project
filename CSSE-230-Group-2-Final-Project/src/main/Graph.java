02bc3a5035e66ac25f437059b8729e0bc156ccf5
Jaden <Jaden@137.112.141.175>
2021-05-13 22:03:18 -0400

    Implementing graph, working on calculating distance

--- CSSE-230-Group-2-Final-Project/src/main/Graph.java
+++ CSSE-230-Group-2-Final-Project/src/main/Graph.java
@@ -1,5 +1,34 @@
 package main;
 
-public class Graph {
+import java.util.ArrayList;
 
+public class Graph {
+	private ArrayList<Node> nodes;
+
+	public Graph() {
+		this.nodes = new ArrayList<Node>();
+	}
+
+	public boolean addNode(String name, float longitude, float latitude, ArrayList<Node> adjacentNodes) {
+		Node n = new Node(name, longitude, latitude, adjacentNodes);
+		nodes.add(n);
+		for (int i = 0; i < adjacentNodes.size(); i++) {
+			adjacentNodes.get(i).adjacentNodes.add(n);
+		}
+		return true;
+	}
+
+	public class Node {
+		private String name;
+		private float longitude;
+		private float latitude;
+		private ArrayList<Node> adjacentNodes;
+
+		public Node(String name, float longitude, float latitude, ArrayList<Node> adjacentNodes) {
+			this.name = name;
+			this.longitude = longitude;
+			this.latitude = latitude;
+			this.adjacentNodes = new ArrayList<Node>();
+		}
+	}
 }
--- CSSE-230-Group-2-Final-Project/src/main/Main.java
+++ CSSE-230-Group-2-Final-Project/src/main/Main.java
@@ -2,4 +2,13 @@ package main;
 
 public class Main {
 
+	public static void main(String[] args) {
+		new Main();
+	}
+	
+	public Main() {
+		
+		
+	}
+	
 }
