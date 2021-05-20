46c7ff13785ee38e8a99c73344594ab5a8a43a29
hverrill <52667642+hverrill@users.noreply.github.com>
2021-05-20 11:46:52 -0400

    merge

--- CSSE-230-Group-2-Final-Project/src/main/Main.java
+++ CSSE-230-Group-2-Final-Project/src/main/Main.java
@@ -35,34 +35,7 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-//		
-//		g.addNode("A", 41, 100);
-//		g.addNode("B", 41, 95);
-//		g.addNode("C", 38, 93);
-//		g.addNode("D", 39, 98);
-//		g.addNode("E", 37, 101);
-//		g.addNode("F", 40, 91);
-//		
-//		g.addNode("G", 2, 40);
-//		g.addNode("lhkj", 98, 35);
-//		g.addNode("325hrer", 2, 40);
-//		g.addNode("ewerdsf", -70, -170);
 
-				
-//		System.out.println("all nodes adjacent to A");
-//		g.printAllAdjNodes(g.nodes.get(0));
-//		System.out.println("all nodes adjacent to B");
-//		g.printAllAdjNodes(g.nodes.get(1));
-//		System.out.println("all nodes adjacent to C");
-//		g.printAllAdjNodes(g.nodes.get(2));
-//		System.out.println("all nodes adjacent to D");
-//		g.printAllAdjNodes(g.nodes.get(3));
-//		System.out.println("all nodes adjacent to E");
-//		g.printAllAdjNodes(g.nodes.get(4));
-//		System.out.println("all nodes adjacent to F");
-//		g.printAllAdjNodes(g.nodes.get(5));
-		
 		//Starting GUI
 		JFrame mapFrame = new JFrame();
 		mapFrame.setTitle("Airports of the World!");
--- CSSE-230-Group-2-Final-Project/src/main/MapComponent.java
+++ CSSE-230-Group-2-Final-Project/src/main/MapComponent.java
@@ -23,7 +23,6 @@ public class MapComponent extends JPanel{
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
 		g.drawImage(Main.map, 0, 0, null);
 		g2d.translate(300, Main.MAP_HEIGHT);
 		
@@ -35,10 +34,9 @@ public class MapComponent extends JPanel{
 		
 		double latMulti = Main.MAP_HEIGHT/180.00, lonMulti = Main.MAP_WIDTH/360.00;
 		for(Node node : this.nodes) {
-
 			int x1 = (int) Math.round(lonMulti*node.longitude);
 			int y1 = -(int) Math.round(latMulti*node.latitude);
-			
+			Color c = Color.BLACK;			
 			//THIS CODE DRAWS LINES FOR EVERYTHING
 //			for(Node neighbor : node.adjacentNodes) {
 //				int x2 = (int) Math.round(lonMulti*neighbor.longitude);
